198120f978313b4b1517ccfb939c88c896a8f8a7
Cool_boy <128267154+Cool-boy-wow@users.noreply.github.com>
2024-03-29 18:04:19 +0300

    Update main.py

--- new/main.py
+++ new/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 import logging
-from github3 import GitHub
+from gitlab import Gitlab
 
 app = FastAPI()
 
@@ -9,10 +9,9 @@ logging.basicConfig(filename='events.log', level=logging.INFO)
 @app.post("/webhook")
 async def webhook(request: Request):
     body = await request.json()
-    event_type = request.headers.get('X-GitHub-Event')
-    if event_type == 'push':
+    event_type = request.headers.get('X-Gitlab-Event')
+    if event_type == 'Push Hook':
         logging.info(f"Push event detected: {body}")
-    elif event_type == 'merge':
-        logging.info(f"Merge event detected: {body}")
+    elif event_type == 'Merge Request Hook':
+        logging.info(f"Merge Request event detected: {body}")
     return {"status": "ok"}
-    
